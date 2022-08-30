3ba87853cfa86f368d4aebb6d374d3bdaa73463b
arrogantant <david991129@naver.com>
2022-08-30 23:33:10 +0900

    텍스트RPG 직업고르기

---
+++ textRPG Choosing a Job/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CSharp
+{
+    class Program
+    {
+        enum ClassType
+        {
+            None = 0,
+            knight = 1,
+            Ardher = 2,
+            Mage = 3,
+            cat = 4,
+        }
+
+        static ClassType ChooseClass()
+        {
+            Console.WriteLine("직업을 선택하세여");
+            Console.WriteLine("[1]기사");
+            Console.WriteLine("[2]궁수");
+            Console.WriteLine("[3]법사");
+            Console.WriteLine("[4]고양이");
+
+            ClassType choice = ClassType.None;
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    choice = ClassType.knight;
+                    break;
+                case "2":
+                    choice = ClassType.Ardher;
+                    break;
+                case "3":
+                    choice = ClassType.Mage;
+                    break;
+                case "4":
+                    choice = ClassType.cat;
+                    break;
+
+
+            }
+
+            return choice;
+        }
+
+            static void Main(string[] args)
+            {
+
+
+                while (true)
+                {
+
+                    ClassType choice = ChooseClass();
+                    if (choice != ClassType.None)
+                        break;
+
+                }
+            }
+        }
+    }
