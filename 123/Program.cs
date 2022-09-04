c63a254f127db7a38d91e8faf7355cb816630549
arrogantant <david991129@naver.com>
2022-09-04 23:49:34 +0900

    유니티 3d
    카메라 플레이어 이동

---
+++ 123/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody charRigidbody;
+
+    void Start()
+    {
+        charRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 inputDir = new Vector3(hAxis, 0, vAxis).normalized;
+
+        charRigidbody.velocity = inputDir * moveSpeed;
+
+        transform.LookAt(transform.position + inputDir);
+    }
+}
+
+public class Camera : MonoBehaviour
+{
+    public Transform targetTransform;
+    public Vector3 CameraOffset;
+
+    void Update()
+    {
+        transform.position = targetTransform.position + CameraOffset;
+    }
+}
