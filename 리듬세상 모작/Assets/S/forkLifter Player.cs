94f0677ed9f8190354b171acfb8edf19c9684bac
arrogantant <david991129@naver.com>
2022-09-28 11:37:30 +0900

    오브젝트 구성

---
+++ 리듬세상 모작/Assets/S/forkLifter Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+namespace RhythmHeavenMania.Games.ForkLifter
+{
+
+    public class forkLifterPlayer : MonoBehaviour
+    {
+        public static forkLifterPlayer instance { get; set;}
+    }
+
+}
